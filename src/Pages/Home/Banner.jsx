6db678fceb3a5700238d2ae3c478ecd9a2ed5c34
Emon <emon.mhk69@gmail.com>
2023-05-10 00:06:27 +0600

    header footer and banner done

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,43 @@
+import img1 from "../../assets/images/banner/1.jpg"
+import img2 from "../../assets/images/banner/2.jpg"
+import img3 from "../../assets/images/banner/3.jpg"
+import img4 from "../../assets/images/banner/4.jpg"
+// import img5 from "../../assets/images/banner/5.jpg"
+// import img6 from "../../assets/images/banner/6.jpg"
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full max-h-screen">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={img1} className="w-full object-fill" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 space-x-3 left-5 right-5 bottom-1">
+                    <a href="#slide4" className="btn btn-circle bg-red-500 border-none">❮</a>
+                    <a href="#slide2" className="btn btn-circle bg-red-500 border-none">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={img2} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 space-x-3 left-5 right-5 bottom-1">
+                    <a href="#slide1" className="btn btn-circle bg-red-500 border-none">❮</a>
+                    <a href="#slide3" className="btn btn-circle bg-red-500 border-none">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={img3} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 space-x-3 left-5 right-5 bottom-1">
+                    <a href="#slide2" className="btn btn-circle bg-red-500 border-none">❮</a>
+                    <a href="#slide4" className="btn btn-circle bg-red-500 border-none">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={img4} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 space-x-3 left-5 right-5 bottom-1">
+                    <a href="#slide3" className="btn btn-circle bg-red-500 border-none">❮</a>
+                    <a href="#slide1" className="btn btn-circle bg-red-500 border-none">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
