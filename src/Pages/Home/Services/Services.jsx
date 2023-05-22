7ca4e7bf3a9e2613dea588dbd2cac885fbe3ea00
Emon <emon.mhk69@gmail.com>
2023-05-22 23:49:33 +0600

    module finish

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,18 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Service from "./Service";
 
 const Services = () => {
 
     const [services, setServices] = useState([]);
     const [asc, setAsc] = useState(true);
+    const [search, setSearch] = useState("");
 
-    useEffect(()=>{
+    const inputRef = useRef(null);
 
-        fetch(`http://localhost:5000/services?sort=${asc ? "asc" : "dec"}`)
-        .then(res=> res.json())
-        .then(data=> setServices(data))
+    useEffect(() => {
 
-    }, [asc]);
+        fetch(`http://localhost:5000/services?search=${search}&sort=${asc ? "asc" : "dec"}`)
+            .then(res => res.json())
+            .then(data => setServices(data))
+
+    }, [asc, search]);
+
+    const handleChange = () => {
+        console.log(inputRef.current.value);
+        setSearch(inputRef.current.value);
+    }
 
     return (
         <div className="my-8">
@@ -23,24 +31,32 @@ const Services = () => {
                     Our Service Area
                 </h2>
                 <p className="text-gray-700">
-                the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. 
+                    the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable.
                 </p>
             </div>
 
-            <button className="btn btn-primary my-4" onClick={()=> setAsc(!asc)}>
-                {asc ? "Sort In Descending Order" : "Sort In Ascending Order"}
-            </button>
+            <div className="my-4 flex justify-between">
+
+                <button className="btn btn-primary " onClick={() => setAsc(!asc)}>
+                    {asc ? "Sort In Descending Order" : "Sort In Ascending Order"}
+                </button>
+
+                <div className="w-96">
+                    <input onChange={handleChange} ref={inputRef} type="text" placeholder="Search here…" className="input input-bordered w-full" />
+                </div>
+
+            </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
 
                 {
 
-                services.map(ser => <Service key={ser._id} service={ser} ></Service>) 
+                    services.map(ser => <Service key={ser._id} service={ser} ></Service>)
 
                 }
 
             </div>
-            
+
         </div>
     );
 };
